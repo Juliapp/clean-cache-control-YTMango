303c2a82517cd0907f5879f22251e49013a11b3c
Juliapp <aragaopintojuli@gmail.com>
2021-03-02 09:28:28 -0300

    ensure LocalSavePurchases create cache with correct timestamp

--- src/data/usecases/save-purchases/local-save-purchases.ts
+++ src/data/usecases/save-purchases/local-save-purchases.ts
@@ -2,9 +2,15 @@ import { CacheStore } from '@/data/protocols/cache'
 import { SavePurchase } from '@/domain/usecases'
 
 export class LocalSavePurchases {
-  constructor(private readonly cacheStore: CacheStore) {}
+  constructor(
+    private readonly cacheStore: CacheStore,
+    private readonly timestamp: Date
+  ) {}
   async save(purchases: Array<SavePurchase.Params>): Promise<void> {
     this.cacheStore.delete('purchases')
-    this.cacheStore.insert('purchases', purchases)
+    this.cacheStore.insert('purchases', {
+      timestamp: this.timestamp,
+      value: purchases
+    })
   }
 }
